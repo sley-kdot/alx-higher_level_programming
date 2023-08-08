8b243f26f6a2be1a29fa975b27fbd8a118f8d99b
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-08-08 11:01:56 -0700

    changed the conditional statment

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ temp = str(number)
 number = int(temp[-1])
 if number > 5:
     print(f"Last digit of {temp} is {number} and is greater than 5")
-if (number < 6) and not 0:
-    print(f"Last digit of {temp} is {number} and is less than 6 and not 0")
-if number == 0:
+elif number == 0:
     print(f"Last digit of {temp} is {number} and is 0")
+else:
+    print(f"Last digit of {temp} is {number} and is less than 6 and not 0")
