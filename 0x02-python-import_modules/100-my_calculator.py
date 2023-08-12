9c2026153217986f34045a80348b9519d506f2f3
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-08-11 20:11:52 -0700

    fixed the IndexError

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,23 +3,21 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
 
-    a = int(argv[1])
-    b = int(argv[3])
     size = len(argv) - 1
     operators = ["+", "-", "*", "/"]
     if size != 3:
         print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
     elif argv[2] in operators:
-        result = add(a, b)
+        result = add(int(argv[1]), int(argv[3]))
     elif argv[2] in operators:
-        result = sub(a, b)
+        result = sub(int(argv[1]), int(argv[3]))
     elif argv[2] in operators:
-        result = mul(a, b)
+        result = mul(int(argv[1]), int(argv[3]))
     elif argv[2] in operators:
-        result = div(a, b)
+        result = div(int(argv[1]), int(argv[3]))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
-    print("{:d} {} {:d} = {:d}".format(a, argv[2], b, result))
+    print("{} {} {} = {:d}".format(argv[1], argv[2], argv[3], result))
