9534e5c3eac99dc225974c95c411d27b398151b4
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-08-13 15:04:13 -0700

    pycodestyle requirement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     size = len(my_list)
     if my_list:
         my_list.reverse()
-        for  num in range(size):
+        for num in range(size):
             print("{:d}".format(my_list[num]))
