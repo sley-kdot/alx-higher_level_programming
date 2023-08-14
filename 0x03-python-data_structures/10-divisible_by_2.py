b60eb89025f7a830b0fec1993b6d889f69f608fe
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-08-14 01:17:03 -0700

    check 4 test

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
-    size = len(my_list)
-    for i in range(size):
+    for i in my_list:
         if i % 2 == 0:
             new_list.append(True)
         else:
