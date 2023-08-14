47ced345e3557286cf208c92de8d474201a8af42
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-08-14 14:25:38 -0700

    added condition to loop

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    str = ""
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
+            if col != 0:
+                print(" ", end="")
             print("{:d}".format(matrix[row, col]), end=" ")
         print()
