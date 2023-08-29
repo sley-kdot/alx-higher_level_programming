5bed687c9adbbb4035a12c0c21d1f1105cac2702
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-08-28 17:40:53 -0700

    removed some failing logic

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    condition = isinstance(value, int)
     try:
-        if condition:
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     except Exception:
         return False
