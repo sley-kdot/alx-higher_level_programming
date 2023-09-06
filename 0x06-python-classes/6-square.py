01426bc2ab7e5f5b70d3c419d031b100ba9df1a5
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-09-06 15:40:37 -0700

    update for check 17

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -82,11 +82,12 @@ class Square:
         """
         if self.__size == 0:
             print()
-        for i in range(self.__position[1]):
-            print()
-        for i in range(self.__size):
-            for j in range(self.__position[0]):
-                print(" ", end='')
-            for k in range(self.__size):
-                print("#", end='')
-            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(" ", end='')
+                for k in range(self.__size):
+                    print("#", end='')
+                print()
