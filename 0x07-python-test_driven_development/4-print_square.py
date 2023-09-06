3528d867eed4aa11a0e19df9164aaf024a497864
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-09-06 02:59:51 -0700

    fix: case 0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -18,8 +18,5 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if type(size) is float and size < 0:
         raise TypeError("size must be an integer")
-    elif size == 0:
-        print()
-    else:
-        for _ in range(size):
-            print("#" * size)
+    for _ in range(size):
+        print("#" * size)
