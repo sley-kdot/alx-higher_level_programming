21123a79b3550fd5ee17ddc111d7e387effc369d
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-09-12 15:30:47 -0700

    fix: missing import module

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """
 module contains a function that returns the JSON representation of an object
 """
+import json
 
 
 def to_json_string(my_obj):
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """a function that returns an object represented by a JSON string"""
+import json
 
 
 def from_json_string(my_str):
