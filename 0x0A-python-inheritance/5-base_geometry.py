756a951558eafeb326c564be66459df5d3f05e87
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-09-11 18:56:35 -0700

    feat: completed task 5 and 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module contains a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """an empty class BaseGeometry"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module contains a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """a class BaseGeometry"""
+
+    def area(self):
+        """function area() that is not fully implemented"""
+        raise Exception("area() is not implemented")
