28be375d3702ee81c07ee2d7d0df6bb0f7e0cd5a
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-09-12 17:04:09 -0700

    style: removed some comments

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,15 +6,7 @@ module that contains a Student class
 
 class Student:
     """a class Student that defines a student"""
-
     def __init__(self, first_name, last_name, age):
-        """initialized class attributes
-
-        Args:
-            first_name (str): first name of student
-            last_name (str): last name of stdent
-            age (int): age of student
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
