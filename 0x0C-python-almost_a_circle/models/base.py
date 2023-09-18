4cee453850f4c61da62505cd9bc8b743f6a695d7
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-09-18 14:08:40 -0700

    style: made changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,15 +44,16 @@ class Base:
         new_list = []
         if list_objs is None:
             return "[]"
-        for obj in list_objs:
-            new_list.append(obj.to_dictionary())
-        with open(name, 'w', encoding="utf-8") as file:
-            file.write(cls.to_json_string(new_list))
+        else:
+            for obj in list_objs:
+                new_list.append(obj.to_dictionary())
+            with open(name, 'w', encoding="utf-8") as file:
+                file.write(cls.to_json_string(new_list))
 
     @staticmethod
     def from_json_string(json_string):
         """Returns the list of the JSON string representation json_string"""
-        if json_string is None or json_string == "":
+        if json_string is None or json_string is []:
             return "[]"
         else:
             return json.loads(json_string)
