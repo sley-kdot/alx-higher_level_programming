114c4f171e86550231fcbd7adc90f346d63b32a7
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-09-18 18:06:31 -0700

    fix: corrected the incorrect file path for the import module

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """module contains a Rectangle class"""
-BaseGeometry = __import__('5-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
