4466fc64a3066e4ba8bf54ac3a771e6efa6d1ae8
Kingsley Okonmah <nnamdiokonm@gmail.com>
2023-12-12 14:23:50 -0800

    fix: sorted in descending order

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,4 @@
 -- displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-ORDER BY avg_temp;
+ORDER BY avg_temp DESC;
