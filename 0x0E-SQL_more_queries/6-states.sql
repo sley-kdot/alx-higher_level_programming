3658906615bffefa8cfb7d35cfeb163141e8a76a
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-12-13 18:49:43 -0800

    style: added comments

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
+-- use database
 USE hbtn_0d_usa;
-
+-- create table
 CREATE TABLE IF NOT EXISTS states (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	name varchar(256) NOT NULL,
