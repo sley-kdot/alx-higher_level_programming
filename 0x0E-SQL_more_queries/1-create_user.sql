f591acee40daa4d84e4471978762bb51f6858d09
Kingsley Okonmah <nnamdiokonm@gmail.com>
2023-12-13 17:49:32 -0800

    fix: added authentication socket

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'user_0d_1_pwd';
