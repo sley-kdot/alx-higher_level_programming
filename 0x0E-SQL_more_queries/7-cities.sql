b06cadefe3101c1bffb8b51db080091e0a694e7d
Kingsley Okonmah <nnamdiokonm@gmail.com>
2023-12-13 18:43:38 -0800

    fix: made changes #0

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,8 +5,8 @@ USE hbtn_0d_usa;
 -- creats a table
 CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT,
-	state_id INT NOT,
+	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (state_id) REFERENCES states(id)
-	);
+);
