f9791996b3bdbe4ab680965d9c71e44159685e8f
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-12-14 16:37:38 -0800

    style: added whitespace for proper indention

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,7 @@
 -- lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
-FROM states
-INNER JOIN cities ON states.id = cities.state_id
-ORDER BY cities.id DESC;
+	FROM states
+	INNER JOIN cities 
+	ON states.id = cities.state_id
+	GROUP BY cities.id
+	ORDER BY cities.id DESC;
