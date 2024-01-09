f4a32dbdd2f5035f2cba73652e1b3f77793dc9e5
Kingsley Okonmah <nnamdiokonma@gmail.com>
2024-01-09 11:34:25 +0100

    fix: added a condition to handle invalid arguments passed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,12 +3,16 @@ const args = process.argv;
 
 const number = parseInt(args[2]);
 let str = '';
-for (let i = 1; i <= number; i++) {
-  for (let j = 1; j <= number; j++) {
-    str += 'X';
+if (isNaN(parseInt(number))) {
+  console.log('Missing size');
+} else {
+  for (let i = 1; i <= number; i++) {
+    for (let j = 1; j <= number; j++) {
+      str += 'X';
+    }
+    if (i < number) {
+      str += '\n';
+    }
   }
-  if (i < number) {
-    str += '\n';
-  }
+  console.log(str);
 }
-console.log(str);
