7caf1db124dbabbe489a07acad8d864dc8bcc5bf
Kingsley Okonmah <nnamdiokonma@gmail.com>
2024-01-15 20:02:56 +0100

    style: added docstrings

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""a script that lists all State objects from the database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
