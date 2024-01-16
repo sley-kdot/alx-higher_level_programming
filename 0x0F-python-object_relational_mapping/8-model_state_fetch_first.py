aacf865ffadb45000005da9a006fb36532a82b40
Kingsley Okonmah <nnamdiokonma@gmail.com>
2024-01-16 21:35:15 +0100

    style: made changes to docstring

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""a script that prints the first State object from the database hbtn_0e_6_usa"""
+"""
+a script that prints the first State object from the database hbtn_0e_6_usa
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
