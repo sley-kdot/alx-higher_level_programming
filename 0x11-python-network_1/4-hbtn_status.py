a4c12fa24862faef256b00d1082e3d52c9f2ecfe
Kingsley Okonmah <nnamdiokonma@gmail.com>
2024-02-05 00:57:49 +0100

    feat: a script that fetches a URL

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module includes a script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/statuss")
+    status = response.text
+    print("Body response:")
+    print(f"\t- type: {type(status)}")
+    print(f"\t- content: {status}")
