cd292c17fa7a0ea1512cfa224241f50f6328f6b7
Kingsley Okonmah <nnamdiokonma@gmail.com>
2024-02-05 01:05:14 +0100

    fix: typo error in the URL

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,8 +5,9 @@ module includes a script that fetches https://alx-intranet.hbtn.io/status
 import requests
 
 if __name__ == "__main__":
-    response = requests.get("https://alx-intranet.hbtn.io/statuss")
+    response = requests.get("https://alx-intranet.hbtn.io/status")
     status = response.text
     print("Body response:")
     print(f"\t- type: {type(status)}")
     print(f"\t- content: {status}")
+
