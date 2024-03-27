303cf946ec9b028ec60cbd6a400a3ff88c13731c
Kingsley Okonmah <nnamdiokonma@gmail.com>
2024-03-27 01:30:52 +0100

    fix: removed usage condition

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,11 +4,6 @@ const args = process.argv;
 const fileName = args[2];
 const text = args[3];
 
-if (args.length < 4) {
-  console.log('USAGE: ./1-writeme.js file_name file_content');
-  process.exit(1);
-}
-
 fs.writeFile(fileName, text, 'utf-8', (err) => {
   if (err) {
     console.log(err);
