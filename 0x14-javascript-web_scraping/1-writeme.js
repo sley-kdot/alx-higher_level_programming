d2bbdbdc347c9a0260377766246013ba7581c486
Kingsley Okonmah <nnamdiokonma@gmail.com>
2024-03-27 01:41:08 +0100

    fix: encoding with utf-8

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,12 @@ const args = process.argv;
 const fileName = args[2];
 const text = args[3];
 
-fs.writeFile(fileName, text, 'utf-8', (err) => {
+// if (args.length < 4) {
+//   console.lo('USAGE: ./1-writeme.js file_name file_content');
+//   process.exit(1);
+// }
+
+fs.writeFile(fileName, text, { encoding: 'utf-8' }, (err) => {
   if (err) {
     console.log(err);
   } else {
