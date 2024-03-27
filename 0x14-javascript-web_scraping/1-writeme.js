1842e5a6a76281566ed83803f80b9e0b02b26932
Kingsley Okonmah <nnamdiokonma@gmail.com>
2024-03-27 01:57:01 +0100

    fix: removed else condition

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,14 +5,12 @@ const fileName = args[2];
 const text = args[3];
 
 // if (args.length < 4) {
-//   console.lo('USAGE: ./1-writeme.js file_name file_content');
+//   console.log('USAGE: ./1-writeme.js file_name file_content');
 //   process.exit(1);
 // }
 
 fs.writeFile(fileName, text, { encoding: 'utf-8' }, (err) => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(text);
+    console.error(err);
   }
 });
