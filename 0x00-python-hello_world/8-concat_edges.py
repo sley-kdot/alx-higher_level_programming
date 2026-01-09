0f4ffc542bb1008c6d93ebf23fa61ec167ee774f
Nnamdi Okonmah <nnamdiokonma@gmail.com>
2026-01-09 21:54:19 +0100

    completed task 6-9

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+first_word, second_word, third_word = str[39:66], str[-22:-18], str[0:7]
+print(f"{first_word} {second_word} {third_word}")
