2f573daab0c61752b3202954fe0fdbb52da3a150
Nnamdi Okonmah <nnamdiokonma@gmail.com>
2026-01-09 22:04:55 +0100

    fix: output failure

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str2 = "ALX"
-print(f"Welcome to {str1.replace(str1, str2)}!")
+str1 = str2.replace("School", "ALX") 
+print(f"Welcome to {str1}!")
